0e81a0542b41e125184270a44032be4e2e8a75ff
Michael Ward <michael24ward@gmail.com>
2020-02-03 15:20:23 +0000

    Added attributes for constants to: left/right ranges, velocities.

--- src/simple_avoidance.py
+++ src/simple_avoidance.py
@@ -13,6 +13,22 @@ class Turtlebot:
         self.pub = rospy.Publisher("/cmd_vel", Twist, queue_size=5)
         self.sub = rospy.Subscriber("/scan", LaserScan, self.callback_scan)
 
+        self.MOVE_VEL = 0.1
+        self.TURN_VEL = 0.5
+
+        # Ranges for the real robot.
+        #self.RIGHT_RANGE = range(355, 400)
+        #self.LEFT_RANGE = range(400, 445)
+
+        # Ranges for the simulation in Gazebo
+        self.RIGHT_RANGE = range(0, 30)
+        self.LEFT_RANGE = range(330, 360)
+
+        # Ranges for the simulation in Robot Ignite Academy
+        # self.RIGHT_RANGE = range(0, 30)
+        # self.LEFT_RANGE = range(690, 720)
+
+
     # Callback function - takes left and right scan data and process it.
     def callback_scan(self, scan):
         sensor_data = scan.ranges
@@ -27,21 +43,21 @@ class Turtlebot:
             cmd = Twist()
 
             # Check right side.
-            for i in range(355, 400):
+            for i in self.RIGHT_RANGE:
                 if sensor_data[i] < 0.3:
                     obstacle = True
 
             # Check left side
-            for i in range(400, 445):
+            for i in self.LEFT_RANGE:
                 if sensor_data[i] < 0.3:
                     obstacle = True
 
             # Decide whether to turn or continue straight
             if obstacle:
                 cmd.linear.x = 0
-                cmd.angular.z = 1.0
+                cmd.angular.z = self.TURN_VEL
             else:
-                cmd.linear.x = 0.1
+                cmd.linear.x = self.MOVE_VEL
                 cmd.angular.z = 0
 
             # Send command to robot
